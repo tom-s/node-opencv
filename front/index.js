6849f1a81a4ad7bdeba8b46239c8dfbcd0b74237
tom-s <thom.schell@gmail.com>
2018-02-04 00:48:17 +0100

    websocket emit video

--- front/index.js
+++ front/index.js
@@ -1,1 +1,68 @@
-console.log("youpi")
+import io from 'socket.io-client'
+
+// Polyfill
+window.navigator.getMedia = (window.navigator.getUserMedia || window.navigator.webkitGetUserMedia || window.navigator.mozGetUserMedia)
+
+// Variables
+let socket, canvas, ctx, video, ticks = 0
+
+// Constants
+const BACK_URL = 'http://localhost:8080'
+const JPG_QUALITY = 0.6
+const CANVAS_WIDTH = 640
+const CANVAS_HEIGHT = 480
+const FRAMES_X = 150 // calculate every x frames
+const CONSTRAINTS = {
+  audio: false,
+  video: true,
+  advanced: [{
+    facingMode: "environment"
+  }]
+}
+
+// Init
+
+window.addEventListener('load', () => {
+  // Connect Web Socket
+  socket = io.connect(BACK_URL)
+  socket.on('connect', data => {
+    console.log("Web Socket connected")
+
+    // Start stream
+    window.navigator.mediaDevices
+      .getUserMedia(CONSTRAINTS)
+      .then(handleSuccess)
+      .catch(handleError)
+  })
+})
+
+const tick = () => {
+  ticks++
+  window.requestAnimationFrame(tick)
+  if (ticks % FRAMES_X !== 0) return
+  console.log("tick")
+
+ 	// Draw video to canvas (avoidable ?)
+  ctx.drawImage(video, 0, 0, CANVAS_WIDTH, CANVAS_HEIGHT)
+
+  // Emit image
+  const dataUrl = canvas.toDataURL('image/jpeg', JPG_QUALITY)
+  socket.emit('img', dataUrl);
+}
+
+
+const handleSuccess = (stream) => {
+  video = document.getElementById('video')
+  canvas = document.getElementById('canvas')
+  ctx = canvas.getContext('2d')
+  canvas.width = CANVAS_WIDTH
+  canvas.height = CANVAS_HEIGHT
+  window.stream = stream; // make stream available to browser console
+  video.srcObject = stream
+  window.requestAnimationFrame(tick)
+}
+
+
+const handleError = (error) => {
+  console.log('navigator.getUserMedia error: ', error);
+}
--- src/index.js
+++ src/index.js
@@ -7,9 +7,13 @@ import initializeDb from './db'
 import middleware from './middleware'
 import api from './api'
 import config from './config.json'
+import io from 'socket.io'
 
 let app = express()
 app.server = http.createServer(app)
+
+// Create websocket
+const ws = io(app.server)
 
 // logger
 app.use(morgan('dev'))
@@ -30,8 +34,23 @@ initializeDb( db => {
 	app.use(middleware({ config, db }))
 
 	// api router
-	app.use('/api', api({ config, db }))
+	//app.use('/api', api({ config, db }))
 
+	// WS
+	ws.on('connection', client => {
+    console.log('Client connected...');
+
+    client.on('join', function(data) {
+      console.log(data);
+    })
+
+    client.on('messages', function(data) {
+      client.emit('broad', data);
+      client.broadcast.emit('broad',data);
+    })
+	})
+
+	// Server
 	app.server.listen(process.env.PORT || config.port, () => {
 		console.log(`Started on port ${app.server.address().port}`)
 	})
