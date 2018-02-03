6ab4d3d713b3cbe5b97a96d64da5711f9a66c0c6
tom-s <thom.schell@gmail.com>
2018-02-04 00:02:42 +0100

    detect rectangle !

--- src/api/rectangle.js
+++ src/api/rectangle.js
@@ -1,60 +1,37 @@
+/* Inspired from
+https://pythontips.com/2015/03/11/a-guide-to-finding-books-in-images-using-python-and-opencv/
+*/
+
 import cv from 'opencv4nodejs'
 
 const GAUSSIAN_SIZE = new cv.Size(3, 3)
+const KERNEL_SIZE = new cv.Size(7, 7)
+const CONTOUR_COLOR = new cv.Vec(0, 255, 0)
 
 const rectangleHandler = async (req, res) => {
+  console.time('start')
   const img = await cv.imreadAsync(`${__dirname}/example.jpg`)
-  let gray = await img.cvtColorAsync (cv.COLOR_BGR2HLS)
+  let gray = await img.bgrToGray()
   gray = await gray.gaussianBlurAsync(GAUSSIAN_SIZE, 0)
   const edged = await gray.cannyAsync(10, 250)
+  const kernel = cv.getStructuringElement(cv.MORPH_RECT, KERNEL_SIZE)
+  const closed = await edged.morphologyExAsync(kernel, cv.MORPH_CLOSE)
+  const contours = await closed.copy().findContoursAsync(cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
 
-  res.send({
-    edged
-  })
+  const result = contours.reduce((memo, contour) => {
+    const peri = contour.arcLength(true)
+    const approx = contour.approxPolyDP(0.02 * peri, true)
+    return approx.length === 4
+      ? {
+        total: memo.total + 1,
+        contours: [...memo.contours, approx]
+      } : memo
+  }, {total: 0, contours: []})
+
+  // Write result image
+  await cv.imwriteAsync(`${__dirname}/result.jpg`, gray)
+  console.timeEnd('start')
+  res.send(result)
 }
 
 export default rectangleHandler
-
-/*
-https://pythontips.com/2015/03/11/a-guide-to-finding-books-in-images-using-python-and-opencv/
-# load the image, convert it to grayscale, and blur it
-image = cv2.imread("example.jpg")
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-gray = cv2.GaussianBlur(gray, (3, 3), 0)
-cv2.imshow("Gray", gray)
-cv2.waitKey(0)
-
-# detect edges in the image
-edged = cv2.Canny(gray, 10, 250)
-cv2.imshow("Edged", edged)
-cv2.waitKey(0)
-
-# construct and apply a closing kernel to 'close' gaps between 'white'
-# pixels
-kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (7, 7))
-closed = cv2.morphologyEx(edged, cv2.MORPH_CLOSE, kernel)
-cv2.imshow("Closed", closed)
-cv2.waitKey(0)
-
-# find contours (i.e. the 'outlines') in the image and initialize the
-# total number of books found
-(cnts, _) = cv2.findContours(closed.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-total = 0
-
-# loop over the contours
-for c in cnts:
-# approximate the contour
-peri = cv2.arcLength(c, True)
-approx = cv2.approxPolyDP(c, 0.02 * peri, True)
-
-# if the approximated contour has four points, then assume that the
-# contour is a book -- a book is a rectangle and thus has four vertices
-if len(approx) == 4:
-cv2.drawContours(image, [approx], -1, (0, 255, 0), 4)
-total += 1
-
-# display the output
-print "I found {0} books in that image".format(total)
-cv2.imshow("Output", image)
-cv2.waitKey(0)
-*/
